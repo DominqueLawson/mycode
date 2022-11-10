8fd1f685f8d8feffac0c0d3fba89eca65e786309
DominqueLawson <dominque.lawson115@gmail.com>
2022-11-10 19:09:01 +0000

    Learning about conditional if

---
+++ challenges/input_print_challenge.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Alta3 Research | RZFeeser
+    print and input challenge """
+
+def main(): 
+
+    # this will collect user's name
+    user_name = input("Enter name:")
+
+    # this will collect day of the week
+    weekday = input("Enter the day of the week:")
+
+    print("Hello,",user_name,"! Happy",weekday,"!")
+    
+main()
